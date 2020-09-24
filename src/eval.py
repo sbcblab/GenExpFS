5f08305d3f90bd804fa022ca51876e6fb80fef84
MatheusBarbieri <mcb_tk@hotmail.com>
2020-09-24 20:39:35 -0300

    refactor: make evaluation more generic by injecting models

--- src/eval.py
+++ src/eval.py
@@ -1,16 +1,21 @@
 from .metrics import one_vs_all_roc_auc
 
-from sklearn.linear_model import SVC
 from sklearn.metrics import make_scorer, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.model_selection import StratifiedKFold, cross_validate
 from sklearn.preprocessing import minmax_scale
 
 
-def classification_eval(X, y):
+def _eval(X, y, model, scoring):
     X = minmax_scale(X)
-    t_svc = SVC()
+    cv = StratifiedKFold()
 
-    scoring = {
+    results = cross_validate(model, X, y, cv=cv, scoring=scoring)
+
+    avg_results = {k.replace("test_", ""): v.mean() for (k, v) in results.items() if not k.endswith("time")}
+    return avg_results
+
+
+scoring = {
         "accuracy": make_scorer(accuracy_score),
         "macro_recall": make_scorer(recall_score, average='macro'),
         "macro_precision": make_scorer(precision_score, average='macro', zero_division=False),
@@ -19,8 +24,6 @@ def classification_eval(X, y):
         "roc_auc": make_scorer(one_vs_all_roc_auc)
     }
 
-    cv = StratifiedKFold()
-    results = cross_validate(t_svc, X, y, cv=cv, scoring=scoring)
-    avg_results = {k.replace("test_", ""): v.mean() for (k, v) in results.items() if not k.endswith("time")}
 
-    return avg_results
+def classification_eval(X, y, models_with_name, scoring=scoring):
+    return {m[1]: _eval(X, y, m[0], scoring) for m in models_with_name}
