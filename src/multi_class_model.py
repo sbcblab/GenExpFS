17510cc201bead3e99b181627f6c17439dcfba92
MatheusBarbieri <mcb_tk@hotmail.com>
2020-09-24 20:58:32 -0300

    feat: pass kwargs and run only once if binary

--- src/multi_class_model.py
+++ src/multi_class_model.py
@@ -6,24 +6,31 @@ class MultiClassModel():
     def __init__(self, model_cls, *args, **kwargs):
         self._model_cls = model_cls
         self._model_args = (args, kwargs)
-        self._models = []
-        self._fit = False
+        self._already_fit = False
 
-    def fit(self, X, y, sample_weight=None, check_input=True):
-        if self._fit:
+    def _fit(self, X, y, importances, **kwargs):
+        args_from_model, kwargs_from_model = self._model_args
+
+        model = self._model_cls(*args_from_model, **kwargs_from_model)
+        model.fit(X, y, **kwargs)
+
+        coef = model.coef_ if hasattr(model, "coef_") else model.feature_importances_
+        importances.append(coef)
+
+    def fit(self, X, y, **kwargs):
+        if self._already_fit:
             raise Exception('Model is already fit.')
-        self._fit = True
+        self._already_fit = True
 
         importances = []
         classes = np.unique(y)
-        binarized_classes = label_binarize(y, classes).transpose()
 
-        for bin_y in binarized_classes:
-            args, kwargs = self._model_args
-            model = self._model_cls(*args, **kwargs)
-            model.fit(X, bin_y)
-            _importances = model.coef_ if hasattr(model, "coef_") else model.feature_importances_
-            importances.append(_importances)
+        if len(classes) <= 1:
+            self._fit(X, y, importances, **kwargs)
+        else:
+            binarized_classes = label_binarize(y, classes).transpose()
+            for bin_y in binarized_classes:
+                self._fit(X, bin_y, importances, **kwargs)
 
         self.feature_importances_ = np.array(importances)
         self.coef_ = self.feature_importances_
