70943e86b69e1922961dbb50e883c712dd30cc51
MatheusBarbieri <mcb_tk@hotmail.com>
2020-09-29 03:18:50 -0300

    feat: add mutual information for discrete/continuous situations

--- src/statistics.py
+++ src/statistics.py
@@ -1,5 +1,17 @@
 import numpy as np
 from scipy.stats import kruskal
+from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
+
+from features import is_discrete
+
+
+def mutual_information(X, y):
+    mutual_info = mutual_info_classif if is_discrete(y) else mutual_info_regression
+
+    if len(X.shape) == 1:
+        X = np.array([X]).T
+
+    return mutual_info(X, y, discrete_features=is_discrete(X))
 
 
 def single_feature_kruskal_wallis(X, y):
