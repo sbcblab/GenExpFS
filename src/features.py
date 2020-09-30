da7247ce2a2d3255634ef1bf8fd8f2dddae98ea5
MatheusBarbieri <mcb_tk@hotmail.com>
2020-09-30 00:27:41 -0300

    fix: incorrectly classifying binary arrays of int and strings

--- src/features.py
+++ src/features.py
@@ -3,21 +3,30 @@ from sklearn.utils.multiclass import type_of_target
 
 
 def _feature_type(X):
-    t = type_of_target(X)
-    unique = list(np.sort(np.unique(X)))
     t_dtype = str(X.dtype)
+    is_numerical = 'float' in t_dtype or 'double' in t_dtype
+    is_integer = 'int' in t_dtype or 'long' in t_dtype or 'short' in t_dtype
 
-    if t == 'binary' and 'int' in t_dtype and set(unique) <= set([0, 1, -1]):
+    if is_numerical:
+        return 'continuous'
+
+    t = type_of_target(X)
+
+    def unique():
+        return list(np.sort(np.unique(X)))
+
+    if t == 'binary' and (is_integer and set(unique()) <= set([0, 1, -1]) or not is_integer):
         return 'categorical'
 
     elif t == 'multiclass':
-        if 'int' in t_dtype:
-            is_sequence = np.array([x + 1 == y for x, y in zip(unique, unique[1:])]).all()
-            starts_at_0_or_1 = np.min(unique) in [0, 1]
+        if is_integer:
+            unique_values = unique()
+            is_sequence = np.array([x + 1 == y for x, y in zip(unique_values, unique_values[1:])]).all()
+            starts_at_0_or_1 = np.min(unique_values) in [0, 1]
 
             if is_sequence and starts_at_0_or_1:
                 return 'categorical'
-        elif 'float' not in t_dtype:
+        else:
             return 'categorical'
 
     return 'continuous'
