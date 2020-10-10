48e52375780e28d1dbc1a01b3531d4f6b732b2fe
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-10 03:09:03 -0300

    feat: extends mrmr with base selector

--- src/feature_selectors/mrmr.py
+++ src/feature_selectors/mrmr.py
@@ -1,14 +1,12 @@
 import numpy as np
 
-from statistics import mutual_information
+from evaluation.statistics import mutual_information
+from .base_selector import BaseSelector, SelectorKind
 
 
-class MRMRFeatureSelector():
-    def __init__(self, k):
-        self._k = k
-
-        self._support_mask = None
-        self._fitted = False
+class MRMRFeatureSelector(BaseSelector):
+    def __init__(self, n_features):
+        super().__init__(SelectorKind.FILTER, n_features)
 
     def _get_support_mask(self):
         if not self._fitted:
@@ -19,17 +17,19 @@ class MRMRFeatureSelector():
         if self._fitted:
             raise Exception('Model is already fitted.')
 
+        self._X = X
         n_samples, n_features = X.shape
 
-        if self._k > n_features:
+        if self._n_features > n_features:
             raise Exception(
-                f'Number of features to select ({self._k}) is higher than number of features in data ({n_features})'
+                f'Number of features to select ({self._n_features}) is higher '
+                f'than number of features in data ({n_features})'
             )
 
         selected = []
         remaining = list(range(n_features))
 
-        selected_to_remaining_mi = np.zeros((self._k - 1, n_features))
+        selected_to_remaining_mi = np.zeros((self._n_features - 1, n_features))
         selected_to_class_mi = []
 
         features_to_class_mi = np.array(mutual_information(X, y))
@@ -42,9 +42,9 @@ class MRMRFeatureSelector():
         selected_to_class_mi.append(highest_mi)
 
         if verbose:
-            print(f'[1/{self._k}] Selected feat {first_feature} with mRMR: {highest_mi:0.3}')
+            print(f'[1/{self._n_features}] Selected feat {first_feature} with mRMR: {highest_mi:0.3}')
 
-        for num_selected in range(1, self._k):
+        for num_selected in range(1, self._n_features):
 
             remaining_features = X[:, remaining]
             last_selected_feature = X.T[selected[-1]]
@@ -63,21 +63,16 @@ class MRMRFeatureSelector():
             selected_to_class_mi.append(higher_score)
 
             if verbose:
-                print(f'[{num_selected+1}/{self._k}] Selected feat {selected_idx} with mRMR: {higher_score:0.3}')
+                print(
+                    f'[{num_selected+1}/{self._n_features}] Selected'
+                    f' feat {selected_idx} with mRMR: {higher_score:0.3}'
+                )
 
         self._support_mask = np.zeros(n_features, dtype=np.bool)
         self._support_mask[selected] = True
-        self.ranking_ = selected
-        self.mrmr_ = selected_to_class_mi
+        self._selected = selected
+        self._rank = selected
+        self._weights = selected_to_class_mi
         self._fitted = True
 
         return self
-
-    def get_suport(self, indices=False):
-        return self.ranking_ if indices else self._support_mask
-
-    def transform(self, X):
-        return X[:, self.ranking_]
-
-    def fit_transform(self, X, y, verbose=0):
-        return self.fit(X, y, verbose).transform(X)
