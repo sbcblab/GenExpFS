942baacc749ab9e8cc4e8bf62ddebebe86869c7a
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-10 18:17:37 -0300

    feat: select k significant best extends base selector

--- src/feature_selectors/select_k_significant_best.py
+++ src/feature_selectors/select_k_significant_best.py
@@ -1,45 +1,36 @@
 import numpy as np
 
 
-class SelectKSignificantBest:
-    def __init__(self, score_func, k=20, p_threshold=0.05):
+from .base_selector import BaseSelector, ResultType
+
+
+class SelectKSignificantBest(BaseSelector):
+    def __init__(self, score_func, n_features=None, p_threshold=0.05):
+        super().__init__(ResultType.WEIGHTS, n_features)
         self.score_func = score_func
-        self.k = k
         self.p_threshold = p_threshold
-        self.scores_, self.pvalues_ = None, None
+        self._weights, self._p_values = None, None
 
     def fit(self, X, y, **kwargs):
+        if self._is_fitted:
+            raise Exception("Model is already fit!")
+        self._X = X
+        n_samples, n_features = X.shape
+
         score_func_result = self.score_func(X, y, **kwargs)
 
         if isinstance(score_func_result, (list, tuple)):
-            self.scores_, self.pvalues_ = score_func_result
-            self.pvalues_ = np.asarray(self.pvalues_)
+            weights, self._p_values = score_func_result
+            self._p_values = np.asarray(self._p_values)
         else:
-            self.scores_ = score_func_result
-        self.scores_ = np.asarray(self.scores_)
+            weights = score_func_result
+
+        if self._p_values:
+            self._selected = np.argwhere(self._p_values < self.p_threshold)
+        else:
+            self._selected = np.arange(n_features)
+
+        self._weights = np.asarray(weights)[self._selected]
+        self.rank = np.argsort(self._weights)[::-1]
+        self._fitted = True
         return self
-
-    def _p_idx(self):
-        return np.argwhere(self.pvalues_ < self.p_threshold)
-
-    def _scores_idx(self):
-        return np.argsort(self.scores_, kind="mergesort")
-
-    def _best_idx(self):
-        p_idx = self._p_idx()
-        return [idx for idx in self._scores_idx() if idx in p_idx][-self.k:]
-
-    def _get_support_mask(self):
-        mask = np.zeros(self.scores_.shape, dtype=bool)
-        mask[self._best_idx()] = 1
-        return mask
-
-    def get_suport(self, indices=False):
-        mask = self._get_support_mask()
-        return np.where(mask)[0] if indices else mask
-
-    def transform(self, X):
-        return X[:, self._get_support_mask()]
-
-    def fit_transform(self, X, y):
-        return self.fit(X, y).transform(X)
