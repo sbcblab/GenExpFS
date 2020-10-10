3ba3dd55ae409206c84a6743a1a77dcf20dac33e
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-10 18:30:19 -0300

    feat: make multi class model extends base selector

--- src/feature_selectors/multi_class_model.py
+++ src/feature_selectors/multi_class_model.py
@@ -1,36 +1,39 @@
 import numpy as np
 from sklearn.preprocessing import label_binarize
 
+from .base_selector import BaseSelector, ResultType
 
-class MultiClassModel():
-    def __init__(self, model_cls, *args, **kwargs):
+
+class MultiClassModel(BaseSelector):
+    def __init__(self, model_cls: BaseSelector, *args, **kwargs):
+        super().__init__(ResultType.COMPLETE_WEIGHTS)
         self._model_cls = model_cls
         self._model_args = (args, kwargs)
-        self._already_fit = False
 
-    def _fit(self, X, y, importances, **kwargs):
+    def _fit(self, X, y, all_weights, **kwargs):
         args_from_model, kwargs_from_model = self._model_args
 
         model = self._model_cls(*args_from_model, **kwargs_from_model)
         model.fit(X, y, **kwargs)
 
-        coef = model.coef_ if hasattr(model, "coef_") else model.feature_importances_
-        importances.append(coef)
+        features_weights = model.get_weights()
+        all_weights = all_weights.append(features_weights)
 
     def fit(self, X, y, **kwargs):
-        if self._already_fit:
+        if self._fitted:
             raise Exception('Model is already fit.')
-        self._already_fit = True
+        self._fitted = True
 
-        importances = []
+        all_weights = []
         classes = np.unique(y)
 
         if len(classes) <= 2:
-            self._fit(X, y, importances, **kwargs)
+            self._fit(X, y, all_weights, **kwargs)
         else:
             binarized_classes = label_binarize(y, classes).transpose()
             for bin_y in binarized_classes:
-                self._fit(X, bin_y, importances, **kwargs)
+                self._fit(X, bin_y, all_weights, **kwargs)
 
-        self.feature_importances_ = np.array(importances).sum(axis=0)
-        self.coef_ = self.feature_importances_
+        self._weights = np.array(all_weights).sum(axis=0)
+        self._rank = np.argsort(self._weights)[::-1]
+        return self
