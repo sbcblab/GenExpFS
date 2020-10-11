4eedf40a0c156b4057fe32ccc7889769b5835901
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-10 23:13:53 -0300

    feat: add dataset model

---
+++ src/data/dataset.py
@@ -0,0 +1,6 @@
+class Dataset:
+    def __init__(self, path, data, classes, columns):
+        self.path = path
+        self.data = data
+        self.classes = classes
+        self.columns = columns
