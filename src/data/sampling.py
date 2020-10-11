148390ffa773804a2e9ea3868f5d257bf7b6a89c
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-10 23:45:59 -0300

    feat: add bootstrap sampling

---
+++ src/data/sampling.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def bootstrap(X, y=None):
+    n_samples, _ = X.shape
+    selected = np.random.choice(n_samples, n_samples)
+    return (X[selected], y[selected]) if y else X[selected]
