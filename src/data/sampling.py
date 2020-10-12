dbc313f8b278f44b9a59192b429f0c9171492ee0
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-12 20:47:07 -0300

    fix: should check for None

--- src/data/sampling.py
+++ src/data/sampling.py
@@ -4,4 +4,4 @@ import numpy as np
 def bootstrap(X, y=None):
     n_samples, _ = X.shape
     selected = np.random.choice(n_samples, n_samples)
-    return (X[selected], y[selected]) if y else X[selected]
+    return (X[selected], y[selected]) if y is not None else X[selected]
