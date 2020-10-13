6b7b81039fed3134462577d997d931d49b7f01d9
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-12 23:36:41 -0300

    feat: add embedded random forest feature selector

---
+++ src/feature_selectors/random_forest.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+from feature_selectors.base_models.embedded import BaseEmbeddedFeatureSelector
+
+
+class RandomForestFeatureSelector(BaseEmbeddedFeatureSelector):
+    def __init__(self, n_features=None, **kwargs):
+        super().__init__(RandomForestClassifier(**kwargs), 'feature_importances_')
