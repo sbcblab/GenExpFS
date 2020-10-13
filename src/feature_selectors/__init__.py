357dbbaebacbbaae9ffca50fe8e288b722c1821f
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-13 00:37:38 -0300

    feat: expose models on __ini__

---
+++ src/feature_selectors/__init__.py
@@ -0,0 +1,23 @@
+from .decision_tree import DecisionTreeFeatureSelector
+from .genetic_algorithm import GeneticAlgorithmFeatureSelector
+from .lasso import LassoFeatureSelector
+from .mrmr import MRMRFeatureSelector
+from .random_forest import RandomForestFeatureSelector
+from .relieff import ReliefF
+from .ridge import RidgeClassifier
+from .base_models.forward_feature_selector import ForwardFeatureSelector
+from .base_models.k_best import KBestFeatureSelector
+from .base_models.k_significant_best import KSignificantBestFeatureSelector
+
+__all__ = [
+    DecisionTreeFeatureSelector,
+    GeneticAlgorithmFeatureSelector,
+    LassoFeatureSelector,
+    MRMRFeatureSelector,
+    RandomForestFeatureSelector,
+    ReliefF,
+    RidgeClassifier,
+    ForwardFeatureSelector,
+    KBestFeatureSelector,
+    KSignificantBestFeatureSelector
+]
