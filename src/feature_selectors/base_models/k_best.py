1865699c265150bed4310ab68299f4fe716b58aa
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-13 02:01:44 -0300

    fix: sklearn should recieve all string to select all features

--- src/feature_selectors/base_models/k_best.py
+++ src/feature_selectors/base_models/k_best.py
@@ -6,5 +6,6 @@ from .embedded import BaseEmbeddedFeatureSelector
 
 class KBestFeatureSelector(BaseEmbeddedFeatureSelector):
     def __init__(self, score_func, n_features=None, p_threshold=0.05):
-        model = SelectKBest(score_func, n_features, p_threshold)
-        super().__init__(model, 'scores_')
+        sklearn_n_features = n_features if n_features else 'all'
+        model = SelectKBest(score_func, sklearn_n_features, p_threshold)
+        super().__init__(model, 'scores_', n_features=n_features)
