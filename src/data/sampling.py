c79b22fd23ea2b03068cbd88c748adf36f65f52b
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-12 21:04:44 -0300

    fix: make sure bootstrap wont sample with the same seed across multiprocesses

--- src/data/sampling.py
+++ src/data/sampling.py
@@ -1,7 +1,9 @@
+from time import time
 import numpy as np
 
 
 def bootstrap(X, y=None):
     n_samples, _ = X.shape
+    np.random.seed(time())
     selected = np.random.choice(n_samples, n_samples)
     return (X[selected], y[selected]) if y is not None else X[selected]
