49f612c03899f55e0403d4f5d11dfa41281e0278
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-13 01:02:49 -0300

    fix: numpy random seed wont accept floats

--- src/data/sampling.py
+++ src/data/sampling.py
@@ -1,9 +1,12 @@
 from time import time
+import random
+
 import numpy as np
 
 
 def bootstrap(X, y=None):
     n_samples, _ = X.shape
-    np.random.seed(time())
+    random.seed(time())
+    np.random.seed(random.randint(0, int(time())))
     selected = np.random.choice(n_samples, n_samples)
     return (X[selected], y[selected]) if y is not None else X[selected]
