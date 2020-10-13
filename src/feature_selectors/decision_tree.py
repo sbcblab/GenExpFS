6c18b80a78af59b9974357f1e066dd05149fe569
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-12 23:31:12 -0300

    feat: add embedded decision tree feature selector

---
+++ src/feature_selectors/decision_tree.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
+
+
+from feature_selectors.base_models.embedded import BaseEmbeddedFeatureSelector
+
+
+class DecisionTreeFeatureSelector(BaseEmbeddedFeatureSelector):
+    def __init__(self, n_features=None, **kwargs):
+        super().__init__(DecisionTreeClassifier(**kwargs), 'feature_importances_')
