07eda23ec9c768d212f1c9f966f6b3151ce80789
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-15 03:05:48 -0300

    feat: add some logging

--- src/task/runner.py
+++ src/task/runner.py
@@ -3,7 +3,7 @@ from time import time
 
 from data.sampling import bootstrap
 from data.results import Result
-from feature_selectors.base_models.base_selector import ResultType
+from feature_selectors.base_models import ResultType
 from .model import Task
 from util.shared_resources import SharedResources
 
@@ -13,46 +13,62 @@ class TaskRunner():
         self._results_writter = results_writter
 
     def run(self, task: Task):
-        shared_resources = SharedResources.get()
-        datasets = shared_resources['datasets']
-        lock = shared_resources['lock']
+        print(f"Starting task {task.name} for dataset: {task.dataset_name}")
+        try:
+            shared_resources = SharedResources.get()
+            datasets = shared_resources['datasets']
+            lock = shared_resources['lock']
+        except Exception as e:
+            print(f"Failed to get shared resources! Reason: {e}")
+            return
 
-        dataset = datasets.get_dataset(task.dataset_name)
-        X = dataset.get_instances()
-        y = dataset.get_classes()
+        try:
+            dataset = datasets.get_dataset(task.dataset_name)
+            X = dataset.get_instances()
+            y = dataset.get_classes()
 
-        if task.bootstrap:
-            X, y = bootstrap(X, y)
+            if task.bootstrap:
+                X, y = bootstrap(X, y)
+        except Exception as e:
+            print(f"Failed to load dataset `{task.dataset_name}` for task `{task.name}`! Reason: {e}")
+            return
 
         fs = task.feature_selector
 
-        start = time()
-        fs.fit(X, y)
-        time_spent = time() - start
-
-        result_type = fs.get_result_type()
-        if result_type is ResultType.WEIGHTS:
-            values = list(fs.get_weights())
-        elif result_type is ResultType.RANK:
-            values = [int(v) for v in fs.get_rank()]
-        else:
-            values = [int(v) for v in fs.get_selected()]
-
-        num_selected = task.feature_selector._n_features
-
-        result = Result(
-            name=task.name,
-            processing_time=time_spent,
-            dataset_path=dataset.path,
-            num_features=dataset.get_instances_shape()[1],
-            num_selected=num_selected if num_selected else -1,
-            sampling='boostrap' if task.bootstrap else 'none',
-            result_type=result_type.value,
-            values=json.dumps(values)
-        )
-
-        lock.acquire()
         try:
-            self._results_writter.write(result)
-        finally:
-            lock.release()
+            start = time()
+            fs.fit(X, y)
+            time_spent = time() - start
+        except Exception as e:
+            print(f"Failed to fit model for dataset `{task.dataset_name}` and task `{task.name}`! Reason: {e}")
+            raise e
+
+        try:
+            if fs.result_type is ResultType.WEIGHTS:
+                values = list(fs.get_weights())
+            elif fs.result_type is ResultType.RANK:
+                values = [int(v) for v in fs.get_rank()]
+            else:
+                values = [int(v) for v in fs.get_selected()]
+
+            num_selected = task.feature_selector._n_features
+
+            result = Result(
+                name=task.name,
+                processing_time=time_spent,
+                dataset_path=dataset.path,
+                num_features=dataset.get_instances_shape()[1],
+                num_selected=num_selected if num_selected else -1,
+                sampling='boostrap' if task.bootstrap else 'none',
+                result_type=fs.result_type.value,
+                values=json.dumps(values)
+            )
+
+            lock.acquire()
+            try:
+                self._results_writter.write(result)
+                print(f"Task {task.name} done! written results for {task.dataset_name}")
+            finally:
+                lock.release()
+        except Exception as e:
+            print(f"Failed to save results task `{task.name}` and dataset `{task.dataset_name}`! Reason: {e}")
