770eedee5e85e239bf781af1832c45ba44456b03
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-18 22:02:16 -0300

    feat: add coloring to print

--- src/task/runner.py
+++ src/task/runner.py
@@ -7,19 +7,24 @@ from feature_selectors.base_models import ResultType
 from .model import Task
 from util.shared_resources import SharedResources
 
+DEFAULT_COLOR = '\033[39m'
+RED_COLOR = "\033[31m"
+CYAN_COLOR = '\033[36m'
+GREEN_COLOR = '\033[32m'
+
 
 class TaskRunner():
     def __init__(self, results_writter):
         self._results_writter = results_writter
 
     def run(self, task: Task):
-        print(f"Starting task {task.name} for dataset: {task.dataset_name}")
+        print(f"{CYAN_COLOR}Starting task {task.name} for dataset: {task.dataset_name}{DEFAULT_COLOR}")
         try:
             shared_resources = SharedResources.get()
             datasets = shared_resources['datasets']
             lock = shared_resources['lock']
         except Exception as e:
-            print(f"Failed to get shared resources! Reason: {e}")
+            print(f"{RED_COLOR}Failed to get shared resources! Reason: {e}{DEFAULT_COLOR}")
             return
 
         try:
@@ -30,7 +35,10 @@ class TaskRunner():
             if task.bootstrap:
                 X, y = bootstrap(X, y)
         except Exception as e:
-            print(f"Failed to load dataset `{task.dataset_name}` for task `{task.name}`! Reason: {e}")
+            print(
+                f"{RED_COLOR}Failed to load dataset `{task.dataset_name}` "
+                f"for task `{task.name}`! Reason: {e}{DEFAULT_COLOR}"
+            )
             return
 
         fs = task.feature_selector
@@ -40,7 +48,10 @@ class TaskRunner():
             fs.fit(X, y)
             time_spent = time() - start
         except Exception as e:
-            print(f"Failed to fit model for dataset `{task.dataset_name}` and task `{task.name}`! Reason: {e}")
+            print(
+                f"{RED_COLOR}Failed to fit model for dataset `{task.dataset_name}` "
+                f"and task `{task.name}`! Reason: {e}{DEFAULT_COLOR}"
+            )
             raise e
 
         try:
@@ -67,8 +78,11 @@ class TaskRunner():
             lock.acquire()
             try:
                 self._results_writter.write(result)
-                print(f"Task {task.name} done! written results for {task.dataset_name}")
+                print(f"{GREEN_COLOR}Task {task.name} done! written results for {task.dataset_name}{DEFAULT_COLOR}")
             finally:
                 lock.release()
         except Exception as e:
-            print(f"Failed to save results task `{task.name}` and dataset `{task.dataset_name}`! Reason: {e}")
+            print(
+                f"{RED_COLOR}Failed to save results task `{task.name}` and dataset "
+                f"`{task.dataset_name}`! Reason: {e}{DEFAULT_COLOR}"
+            )
