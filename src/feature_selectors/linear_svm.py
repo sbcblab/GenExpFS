956ffc9f7b544f61a76999894bba8b43fc42c212
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-18 23:05:41 -0300

    fix: make linear svm actually linear

--- src/feature_selectors/linear_svm.py
+++ src/feature_selectors/linear_svm.py
@@ -6,4 +6,4 @@ from feature_selectors.base_models.embedded import BaseEmbeddedFeatureSelector
 
 class LinearSVMFeatureSelector(BaseEmbeddedFeatureSelector):
     def __init__(self, n_features=None, **kwargs):
-        super().__init__(SVC(**kwargs), 'coef_', n_features=n_features)
+        super().__init__(SVC(kernel='linear', **kwargs), 'coef_', n_features=n_features)
