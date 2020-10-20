4640933a6d16661fb55c2c588da984b90372de60
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-20 03:30:35 -0300

    fix: lasso should use encoded labels to classification problems

--- src/feature_selectors/lasso.py
+++ src/feature_selectors/lasso.py
@@ -1,9 +1,8 @@
 from sklearn.linear_model import Lasso
 
-
-from feature_selectors.base_models.embedded import BaseEmbeddedFeatureSelector
+from feature_selectors.base_models import BaseEmbeddedFeatureSelector
 
 
 class LassoFeatureSelector(BaseEmbeddedFeatureSelector):
     def __init__(self, n_features=None, **kwargs):
-        super().__init__(Lasso(alpha=0.001, **kwargs), 'coef_', n_features=n_features)
+        super().__init__(Lasso(alpha=0.001, **kwargs), 'coef_', n_features=n_features, encode_classes=True)
