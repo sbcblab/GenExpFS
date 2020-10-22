9605812a0ee49c3807c22bbfbaaf24fd3b314dec
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-21 23:52:43 -0300

    refactor: simplify default scoring

--- src/evaluation/scoring.py
+++ src/evaluation/scoring.py
@@ -5,6 +5,13 @@ from .metrics import one_vs_all_roc_auc
 
 default_scoring = {
     "accuracy": make_scorer(accuracy_score),
+    "macro_f1": make_scorer(f1_score, average='macro'),
+    "roc_auc": make_scorer(one_vs_all_roc_auc)
+}
+
+
+full_scoring = {
+    "accuracy": make_scorer(accuracy_score),
     "macro_recall": make_scorer(recall_score, average='macro'),
     "macro_precision": make_scorer(precision_score, average='macro', zero_division=False),
     "micro_f1": make_scorer(f1_score, average='micro'),
