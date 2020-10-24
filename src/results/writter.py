595f404726ebf528b9ee6231be799ece44f7b005
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-24 14:58:48 -0300

    feat: add save dataframe function

--- src/results/writter.py
+++ src/results/writter.py
@@ -1,6 +1,8 @@
 import csv
 import os
 
+import pandas as pd
+
 from .model import Result
 
 
@@ -9,7 +11,7 @@ class ResultsWritter:
         self._base_path = base_path
         self._file_name = file_name
 
-    def _write_csv(path_to_save, result):
+    def _write_csv(path_to_save, result: Result):
         with open(path_to_save, "a") as f:
             writer = csv.DictWriter(f, result.fields())
 
@@ -33,3 +35,14 @@ class ResultsWritter:
         path_to_save = os.path.join(self._base_path, f"{self._file_name}.csv",)
 
         self._write_csv(path_to_save, result)
+
+    def write_dataframe(self, df: pd.DataFrame, file_name: str):
+        if not os.path.exists(self._base_path):
+            os.makedirs(self._base_path)
+
+        path = os.path.join(self._base_path, file_name)
+
+        if os.path.exists(path):
+            df.to_csv(path, mode='a', index=False, header=False)
+        else:
+            df.to_csv(path, index=False)
