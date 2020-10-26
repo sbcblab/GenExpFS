4b08b6f5d8b525ebb4bc6258f38d0998a179eb0d
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-26 17:51:18 -0300

    refactor: remove unused code and pass chacks to construction

--- src/results/writter.py
+++ src/results/writter.py
@@ -7,9 +7,14 @@ from .model import Result
 
 
 class ResultsWritter:
-    def __init__(self, base_path, file_name='selection'):
-        self._base_path = base_path
+    def __init__(self, base_dir, file_name='selection.csv'):
+        self._base_dir = base_dir
         self._file_name = file_name
+
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+        elif not os.path.isdir(base_dir):
+            raise Exception(f"base_dir[{base_dir}] must be a directory!")
 
     def _write_csv(self, path_to_save, result: Result):
         with open(path_to_save, "a") as f:
@@ -20,27 +25,20 @@ class ResultsWritter:
 
             writer.writerow(result.to_dict())
 
-    def write_csv_per_model_and_dataset(self, result: Result):
-        dir_path = os.path.join(self._base_path, result.name)
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-        path_to_save = os.path.join(dir_path, f"{result.dataset_name}.csv",)
-
-        self._write_csv(path_to_save, result)
-
     def write_csv(self, result: Result):
-        if not os.path.exists(self._base_path):
-            os.makedirs(self._base_path)
+        if not os.path.exists(self._base_dir):
+            os.makedirs(self._base_dir)
 
-        path_to_save = os.path.join(self._base_path, f"{self._file_name}.csv",)
+        file_name = self._file_name if self._file_name.endswith('.csv') else f"{self._file_name}.csv"
+        path_to_save = os.path.join(self._base_dir, file_name)
 
         self._write_csv(path_to_save, result)
 
     def write_dataframe(self, df: pd.DataFrame, file_name: str):
-        if not os.path.exists(self._base_path):
-            os.makedirs(self._base_path)
+        if not os.path.exists(self._base_dir):
+            os.makedirs(self._base_dir)
 
-        path = os.path.join(self._base_path, file_name)
+        path = os.path.join(self._base_dir, file_name)
 
         if os.path.exists(path):
             df.to_csv(path, mode='a', index=False, header=False)
