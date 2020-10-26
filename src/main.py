42b30a02c9b15792d9f2f903d680dd0ba4200133
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-26 02:29:57 -0300

    feat: add stability evaluation to flow

--- src/main.py
+++ src/main.py
@@ -6,10 +6,15 @@ from sklearn.exceptions import ConvergenceWarning
 from data.dataloader import DataLoader
 from data.shared_datasets import SharedDatasets
 from results.writter import ResultsWritter
+from results.loader import ResultsLoader
 from task.runner import TaskRunner
 from util.shared_resources import SharedResources
 from util.command_line import get_args
-from util.task_creation_helper import default_presets
+from util.task_creation_helper import test_presets
+
+from evaluation.results_scorer import ResultsScorer
+from evaluation.results_stability import ResultsStability
+from evaluation.selection import SelectionScorer
 
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -57,10 +62,33 @@ def main():
     results_writter = ResultsWritter(results_path)
     task_runner = TaskRunner(results_writter)
 
-    tasks = default_presets(1)
+    tasks = test_presets()
 
     with Pool(cpu_count(), SharedResources.set_resources, [shared_resources]) as pool:
         pool.map(task_runner.run, tasks)
+
+    results_loader = ResultsLoader(results_path)
+
+    selection_scorer = SelectionScorer()
+    scorer = ResultsScorer(results_loader, datasets, selection_scorer)
+    results = scorer.score_all()
+
+    results_writter.write_dataframe(results, 'scored.csv')
+
+    stability_evaluator = ResultsStability(results_loader)
+
+    alg_stab_sum, alg_stab = stability_evaluator.summarized_algorithms_stability(
+        sampling='none', return_complete=True
+    )
+    results_writter.write_dataframe(alg_stab, 'algorithm_stability.csv')
+    results_writter.write_dataframe(alg_stab_sum, 'summarized_algorithm_stability.csv')
+
+    alg_data_stab_sum, alg_data_stab = stability_evaluator.summarized_algorithms_stability(
+        sampling='bootstrap', return_complete=True
+    )
+
+    results_writter.write_dataframe(alg_data_stab, 'algorithm_to_data_stability.csv')
+    results_writter.write_dataframe(alg_data_stab_sum, 'summarized_algorithm_to_data_stability.csv')
 
 
 if __name__ == '__main__':
