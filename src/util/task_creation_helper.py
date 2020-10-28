15192b0325674815661b8715f01e48d33b4ebd9b
MatheusBarbieri <mcb_tk@hotmail.com>
2020-10-28 02:07:38 -0300

    refactor: simplify presets creation and logging

--- src/util/task_creation_helper.py
+++ src/util/task_creation_helper.py
@@ -55,48 +55,68 @@ def load_preset(path):
 
 def config_to_tasks(config):
     for entry in config:
-        for dataset, alg in product(entry['datasets'], entry['algs']):
-            for params in alg['params']:
-                for _ in range(alg['runs']):
-                    name = alg['name']
+        for dataset, algorithm in product(entry['datasets'], entry['algorithms']):
+            for params in algorithm['params']:
+                for _ in range(algorithm['runs']):
+                    name = algorithm['name']
                     yield Task(name, feature_selectors[name](*params), dataset, False)
-                for _ in range(alg['bootstrap_runs']):
-                    name = alg['name']
+                for _ in range(algorithm['bootstrap_runs']):
+                    name = algorithm['name']
                     yield Task(name, feature_selectors[name](*params), dataset, True)
 
 
-def tasks_from_presets(preset_names, runs=1):
+def print_preset(name, preset, verbose, runs):
+    if verbose > 0:
+        print(f"Loaded configs from preset {name}:")
+        for config in preset:
+
+            description = config.get('description')
+            config_runs = 1 if config.get('run_once') else runs
+
+            alg_runs = [
+                (alg['name'], alg['runs'])
+                for alg in config.get('algorithms')
+                if alg['runs'] > 0
+            ]
+
+            bootstrap_alg_runs = [
+                (alg['name'], alg['bootstrap_runs'])
+                for alg in config.get('algorithms')
+                if alg['bootstrap_runs'] > 0
+            ]
+
+            if description is not None:
+                print(f"\t{description}")
+                print("\t\tDatasets:")
+                [print(f'\t\t\t{d}') for d in config['datasets']]
+                print(f"\t\tPreset repeat times: {config_runs}")
+                print("\t\tAlgorithm runs:")
+                [print(f'\t\t\t{a}: {r} per dataset') for a, r in alg_runs]
+                print("\t\tAlgorithm Bootstrap runs:")
+                [print(f'\t\t\t{a}: {r} per dataset') for a, r in bootstrap_alg_runs]
+
+
+def tasks_from_presets(preset_names, runs=1, verbose=0):
     tasks = []
     for name in preset_names:
         try:
             _name = name if name.endswith('.json') else f'{name}.json'
             preset = load_preset(_name)
+
+            print_preset(name, preset, verbose, runs)
+
+            run_once_configs = [config for config in preset if config.get('run_once')]
+            tasks.append(config_to_tasks(run_once_configs))
+
+            run_n_configs = [config for config in preset if not config.get('run_once')]
             for _ in range(runs):
-                tasks.append(config_to_tasks(preset))
+                tasks.append(config_to_tasks(run_n_configs))
         except Exception as e:
-            if name == 'default':
-                tasks.append(default_tasks(runs))
-            else:
-                print(f"Could not load preset {name}, reason: {e}")
+            print(f"Could not load preset {name} because: {e}")
 
-    return chain(*tasks)
+    final_tasks = list(chain(*tasks))
 
+    if verbose > 0:
+        print(f"Generated {len(final_tasks)} for {preset_names} presets!")
 
-def test_presets():
-    test_config = load_preset('test.json')
-    return config_to_tasks(test_config)
-
-
-def default_tasks(runs=100):
-    det_config = load_preset('default/deterministic.json')
-    bs_det_config = load_preset('default/bootstrap_deterministic.json')
-    stoc_config = load_preset('default/stochastic.json')
-    bs_stoc_config = load_preset('default/bootstrap_stochastic.json')
-
-    tasks = [config_to_tasks(det_config)]
-    for r in range(runs):
-        tasks.append(config_to_tasks(bs_det_config))
-        tasks.append(config_to_tasks(stoc_config))
-        tasks.append(config_to_tasks(bs_stoc_config))
-
-    return chain(*tasks)
+    return final_tasks
